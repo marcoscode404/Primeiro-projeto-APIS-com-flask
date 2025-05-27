1caffb17d3a2c2ef434c001a1e84df0507d3d113
marcoscode404 <viniciusmg99@outlook.com>
2025-05-27 00:23:13 -0400

    feat: primeira rota com flask

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_word():
+    return "Hello world"
+
+# habilita logs do servidor
+if __name__ == "__main__":  ## regra para uso local - DESENVOLVIMENTO
+    app.run(debug=True)
