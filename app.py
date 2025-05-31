d15598b3ee212561ad652039ddcba7139d712aad
marcoscode404 <viniciusmg99@outlook.com>
2025-05-31 00:04:11 -0400

    feat: save

--- app.py
+++ app.py
@@ -1,14 +1,63 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_word():
-    return "Hello world"
+# CRUD
+# Create, Read, Update and Delete
+# Tabela: Tarefa
 
-@app.route("/about")
-def about():
-    return "Página sobre"
+tasks = []
+task_id_control = 1
+
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    global task_id_control
+    data = request.get_json()  # get_son recupera os dados que o cliente envia
+    new_task = Task(
+        id=task_id_control, title=data["title"], description=data.get("description", "")
+    )
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    tasks_list = [task.to_dict() for task in tasks]
+
+    output = {
+        "tasks": tasks_list,
+        "total_tasks": len(tasks_list),
+    }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+    print(task)
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
 
 # habilita logs do servidor
 if __name__ == "__main__":  ## regra para uso local - DESENVOLVIMENTO
